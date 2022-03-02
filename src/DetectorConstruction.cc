522ba6ce6da7d48fb89e298f833e9ed212af7b51
JeongHyeokPark <wjd1004ip@naver.com>
2022-03-02 11:45:43 +0900

    Avoid segmentation fault error and erase unnecessaries in vis.mac

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -38,4 +38,25 @@ DetectorConstruction::~DetectorConstruction()
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {  
+  //Elements
+  // G4Elements( name, symbol, Z number, A )
+  G4Element* elH = new G4Element("H", "H", 1., 1.*g/mole);
+  G4Element* elC = new G4Element("C", "C", 6., 12.*g/mole);
+  G4Element* elN = new G4Element("N", "N", 7., 14.00674*g/mole);
+  G4Element* elO = new G4Element("O", "O", 8., 15.9994*g/mole);
+
+  G4double density = universe_mean_density;
+  // G4Material( name, density, # of consisting atom, state )
+  G4Material* Vacuum = new G4Material("Vacuum", density, 2);
+  Vacuum->AddElement(elN, .7);
+  Vacuum->AddElement(elO, .3);
+
+  // World
+  G4double world_size = 10*m;
+
+  G4Box* solidWorld = new G4Box("World", 0.5*world_size, 0.5*world_size, 0.5*world_size);
+  G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, Vacuum, "World");
+  G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "World", 0, false, -1, true);
+
+  return physWorld;
 }
